5e34938259c6f1f1453178f532b6b89a702bb0e8
Richard Nyande <nyander@aston.ac.uk>
2019-10-21 11:02:49 +0100

    create route for about page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ Route::get('/', function () {
     return view('welcome');
     });
 
-//this is a test to see if the website displays
+/*this is a test to see if the website displays
 Route::get('/richard', function () {
     return 'I can confirm that the routes are working'; 
+});
+*/
+//this route should ensure that when the end user types in website link/about - it should open the about page in the pages folder 
+Route::get('/about', function () {
+    return view('pages.about'); 
 });

017f83c38e330c66d543bd4e3938538d6ad20956
Richard Nyande <nyander@aston.ac.uk>
2019-10-21 12:09:24 +0100

    added content for about page

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -1,1 +1,15 @@
-About
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Yande Gadgets</title> 
+       
+    </head>
+    <body>
+        <div>
+            <h1>The About page works</h1>
+            <p> I can now conirm that you are connected to the index page </p> 
+        </div>
+    </body>
+</html>

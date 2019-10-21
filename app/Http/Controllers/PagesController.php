dff9edccd752a7e99f6f2105b5bdab08101ded77
Richard Nyande <nyander@aston.ac.uk>
2019-10-21 12:12:39 +0100

    added about and  services function

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,13 @@ class PagesController extends Controller
     public function index() {
         return view('pages.index');
     }
+    //this public method/function is available and accessible anywhere, this function will return the index page as a view
+    public function about() {
+        return view('pages.about');
+    }
+
+    //this public method/function is available and accessible anywhere, this function will return the index page as a view
+    public function products() {
+        return view('pages.products');
+    }
 }

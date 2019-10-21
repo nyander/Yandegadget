596a38da05309a5f996e286cc8cc3b1dd52c9f8b
Richard Nyande <nyander@aston.ac.uk>
2019-10-21 11:21:20 +0100

    test of pagecontroller index function

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    //this public method/function is available and accessible anywhere
+    public function index() {
+        return 'THIS IS A TEST FOR INDEX FUNCTION ';
+    }
 }

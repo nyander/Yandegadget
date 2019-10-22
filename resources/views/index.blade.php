8fd57f51f53284c0f8ddc00af3dc2d22d7b04d0c
Richard Nyande <nyander@aston.ac.uk>
2019-10-22 18:17:49 +0100

    extended and added sections

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
-//we ae 
+{{-- extend is basically extending another class using the @extend function which is extending the app file in the layout folder  --}}
 @extends('layouts.app')
-<h1>The index page works</h1>
-<p> I can now conirm that you are connected to the index page hello serve</p> 
+
+{{--inside the @section is what will be added in the app file, basically content to be added to your template--}}
+@section('content')
+    <h1>The index page works</h1>
+    <p> I can now conirm that you are connected to the index page hello serve</p> 
+@endsection
 

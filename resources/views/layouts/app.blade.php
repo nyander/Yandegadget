7ea6c792586e089ba9be840c421ffcf3385821aa
Richard Nyande <nyander@aston.ac.uk>
2019-10-22 18:10:35 +0100

    added yield

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     </head>
     <body>
         <div>
-            <h1>The index page works</h1>
-            <p> I can now conirm that you are connected to the index page hello serve</p> 
+            //this is will return the value and saves the content in its state
+            @yield('content')
         </div>
     </body>
 </html>

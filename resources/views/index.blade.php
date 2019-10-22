227a735c21118a483b450862567119d4d9ee4fa5
Richard Nyande <nyander@aston.ac.uk>
2019-10-22 18:00:59 +0100

    this was to ensure php works

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <body>
         <div>
             <h1>The index page works</h1>
-            <p> I can now conirm that you are connected to the index page </p> 
+            <p> I can now conirm that you are connected to the index page hello serve</p> 
         </div>
     </body>
 </html>

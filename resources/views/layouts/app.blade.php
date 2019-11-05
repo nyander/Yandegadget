a747a91fe9238d5e9bbf38941bfa6de60b83274b
Richard Nyande <nyander@aston.ac.uk>
2019-11-05 12:03:50 +0000

    add css to app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">       
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
-        <title>Yande Gadgets</title>               
+        <meta name="author" content="Richard Nyande">
+        
+        <title>Yande Gadgets</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,600" type="text/css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">               
+    
+        <!--styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         {{--we are including the navbar into the website which is located in the inc folder --}}

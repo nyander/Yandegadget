4274888e070b9aabc311341a0da2ccda9288c3f5
Richard Nyande <nyander@aston.ac.uk>
2019-11-05 10:03:13 +0000

    attempting to make Css work in server 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">        
+        <meta name="viewport" content="width=device-width, initial-scale=1">       
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
         <title>Yande Gadgets</title>               
     </head>
     <body>

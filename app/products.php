5ec15bf9e99ecbc1363d3ae020cc497651403b3d
Richard Nyande <nyander@aston.ac.uk>
2019-11-18 13:00:04 +0000

    updates tables

--- app/products.php
+++ app/products.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class products extends Model
 {
    protected $fillable = ['product_Name','product_Type','product_Cost','product_Supplier','purchase_Date','condition','condition_Notes','selling_Price','recieved','shipment','sold','sold_To'];
 }

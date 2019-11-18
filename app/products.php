2775c0f56bef625fbaffa65d299f4a08d6655c82
Richard Nyande <nyander@aston.ac.uk>
2019-11-18 13:04:41 +0000

    change in field names

--- app/products.php
+++ app/products.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class products extends Model
 {
-   protected $fillable = ['product_Name','product_Type','product_Cost','product_Supplier','purchase_Date','condition','condition_Notes','selling_Price','recieved','shipment','sold','sold_To'];
+   protected $fillable = ['name','type','cost','supplier','purchase_Date','condition','condition_Notes','selling_Price','recieved','shipment','sold','sold_To'];
 }

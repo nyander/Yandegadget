46753d873523f25779447e969743fac918bf4333
Richard Nyande <nyander@aston.ac.uk>
2019-11-18 12:07:45 +0000

    changes to index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,10 @@
 {{--inside the @section is what will be added in the app file, basically content to be added to your template--}}
 @section('content')
 {{-- $title is the variable name in which the Postcontroller will replace with the variable $tite - so this title will be variable in the postcontroller    --}}
-    <h1>{{$title}}</h1>
-    <p> I can now conirm that you are connected to the index page hello serve</p> 
+    <div class="jumbotron text-center"> 
+        <h1>{{$title}}</h1>
+        <p> I can now conirm that you are connected to the index page hello serve</p> 
+        <p><a class=" btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class=" btn btn-primary btn-lg" href="/signup" role="button">Sign-up</a>
+    </div>     
 @endsection
 
